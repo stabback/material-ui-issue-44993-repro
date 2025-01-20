2287c6ba41c0e8e8f6cc1c227c24e8cca424bf47
Diego Andai <diego@mui.com>
2025-01-20 16:36:35 -0300

    Use ESM improvement's PR builds and add missing react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 declare module "@remix-run/node" {
@@ -10,6 +11,7 @@ declare module "@remix-run/node" {
 
 export default defineConfig({
   plugins: [
+    react(),
     remix({
       future: {
         v3_fetcherPersist: true,
